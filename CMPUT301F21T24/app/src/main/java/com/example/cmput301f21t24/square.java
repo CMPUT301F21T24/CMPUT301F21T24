26637c989082021fb35682ab9d064c2dc7507eed
WenhaoCao0729 <77565060+WenhaoCao0729@users.noreply.github.com>
2021-10-04 15:44:15 -0600

    created square.java

---
+++ CMPUT301F21T24/app/src/main/java/com/example/cmput301f21t24/square.java
@@ -0,0 +1,14 @@
+// This file is ectends Shape Class.
+package com.example.cmput301f21t24;
+
+public class square extends Shape{
+    
+    private int side_length;
+    private int area;
+    
+    
+    public square(int side_length){
+        this.side_length = side_length
+        this.area = area;
+    }
+}
